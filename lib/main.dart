9efc408840db0a95db45b61a432e3fd9aa4c635f
idahdam <idahdam.bs@gmail.com>
2021-11-11 14:20:04 +0700

    Final Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:odyssey/pages/favorite.dart';
 import 'package:odyssey/pages/home.dart';
+import 'package:odyssey/pages/profile.dart';
+import 'package:odyssey/pages/transaction.dart';
 
-void main() => runApp(MaterialApp(
-  initialRoute: '/home',
+void main() => runApp(new MaterialApp(
+  initialRoute: '/',
   routes: {
-    // '/': (context) => Loading(),
-    '/home': (context) => Home(),
-    // '/location': (context) => ChooseLocation(),
+    '/': (context) => Home(),
+    '/favorite': (context) => Favorite(),
+    '/transaction': (context) => TransactionPage(),
+    '/profile': (context) => Profile(),
   },
 ));
 
