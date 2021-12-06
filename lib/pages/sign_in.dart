059a25f273e526636fee7619746b1eee25202dc3
ticklingtom <mammamiaadi@gmail.com>
2021-12-06 18:09:21 +0700

    adi: pushing fixed login and signup page

--- lib/pages/sign_in.dart
+++ lib/pages/sign_in.dart
@@ -11,60 +11,84 @@ class SignInScreen extends StatefulWidget {
 
 class InitState extends State<SignInScreen> {
   bool isHiddenPassword = true;
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF21574A),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                //logo
-                Stack(
-                  children: [
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0.0, 25.0, 15.0, 0.0),
-                      alignment: Alignment.topRight,
-                      child: Image.asset('assets/images/white_logo.png', width: 70.0),
-                    ),
-                  ],
-                ),
-                Padding(padding: EdgeInsets.only(bottom: 100)),
-                //text container
-                Container(
+            child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF21574A),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              //logo
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0.0, 25.0, 15.0, 0.0),
+                    alignment: Alignment.topRight,
+                    child: Image.asset('assets/images/white_logo.png',
+                        width: 70.0),
+                  ),
+                ],
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 100)),
+              //text container
+              Container(
                   alignment: Alignment.center,
-                  child:(
-                    RichText(
-                      text: TextSpan(
-                        text: 'Ahoy, Travelers!',
-                        style: TextStyle(
-                            fontFamily: 'KulimPark',
-                            fontWeight: FontWeight.w600,
-                            fontSize: 44,
-                            color: Colors.white),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: '\nSign up before boarding with us',
+                  child: (RichText(
+                    text: TextSpan(
+                      text: 'Welcome Back!',
+                      style: TextStyle(
+                          fontFamily: 'KulimPark',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 44,
+                          color: Colors.white),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: '\nLogin to start your journey',
                             style: TextStyle(
                                 fontFamily: 'Poppins',
                                 fontWeight: FontWeight.w400,
                                 color: Colors.white,
                                 fontSize: 20)),
-                        ],
-                      ),
-                    )
-                  )
-                ),
-                Padding(padding: EdgeInsets.only(bottom: 50)),
-                //email field
-                SizedBox(
+                      ],
+                    ),
+                  ))),
+              Padding(padding: EdgeInsets.only(bottom: 50)),
+              //email field
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                    ),
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: const BorderSide(
+                                color: Colors.white, width: 1.5)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                          color: Colors.white,
+                          width: 3.0,
+                        )),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          fontFamily: 'KulimPark',
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                          color: Colors.white60,
+                        ))),
+              ),
+              //password field
+              SizedBox(
                   width: 300,
                   child: TextFormField(
                       style: TextStyle(
@@ -72,109 +96,82 @@ class InitState extends State<SignInScreen> {
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w400,
                       ),
+                      obscureText: isHiddenPassword,
                       decoration: InputDecoration(
-                          focusedBorder:UnderlineInputBorder(
+                        focusedBorder: UnderlineInputBorder(
                             borderSide: const BorderSide(
-                              color: Colors.white,
-                              width: 1.5)),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 3.0,
-                          )),
-                          labelText: 'Email',
-                          labelStyle: TextStyle(
-                            fontFamily: 'KulimPark',
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20,
-                            color: Colors.white60,
-                          ))),
-                ),
-                //password field
-                SizedBox(
-                    width: 300,
-                    child: TextFormField(
-                        style: TextStyle(
+                                color: Colors.white, width: 1.5)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
                           color: Colors.white,
-                          fontFamily: 'Poppins',
-                          fontWeight: FontWeight.w400,
-                        ),
-                        obscureText: isHiddenPassword,
-                        decoration: InputDecoration(
-                          focusedBorder:UnderlineInputBorder(
-                            borderSide: const BorderSide(
-                              color: Colors.white,
-                              width: 1.5)),
-                          enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                            color: Colors.white,
-                            width: 3.0,
-                          )),
-                          suffixIcon: InkWell(
+                          width: 3.0,
+                        )),
+                        suffixIcon: InkWell(
                             onTap: _togglePasswordView,
-                            child: (isHiddenPassword == true) ? Icon(Icons.visibility,color: Colors.white): Icon(Icons.visibility_off,color: Colors.white)
-                          ),
-                          labelText: 'Password',
-                          labelStyle: TextStyle(
-                            fontFamily: 'KulimPark',
-                            fontWeight: FontWeight.w600,
-                            fontSize: 20,
-                            color: Colors.white60,
-                          ),
-                        ))),
-                Padding(padding: EdgeInsets.only(bottom: 20)),
-                //button submit
-                Container(
-                  margin: EdgeInsets.only(right: 30),
-                  alignment: Alignment.centerRight,
-                  child: MaterialButton(
-                    height: 45,
-                    onPressed: () => {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (BuildContext context) => Main()))
-                    },
-                    child: Image.asset('assets/images/submit.png', width: 45),
-                  ),
-                ),
-                Padding(padding: EdgeInsets.only(bottom: 162)),
-                //Bottom text
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    text: 'Already have an account?',
-                    style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w400,
-                        fontSize: 18,
-                        color: Colors.white),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: '\nLogin',
-                        style: TextStyle(
-                          fontFamily: 'Poppins',
+                            child: (isHiddenPassword == true)
+                                ? Icon(Icons.visibility, color: Colors.white)
+                                : Icon(Icons.visibility_off,
+                                    color: Colors.white)),
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          fontFamily: 'KulimPark',
                           fontWeight: FontWeight.w600,
-                          color: Colors.white,
-                          fontSize: 18,
+                          fontSize: 20,
+                          color: Colors.white60,
                         ),
-                        recognizer: new TapGestureRecognizer()
-                          ..onTap = () => {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (BuildContext context) =>
-                                            SignUpScreen()))
-                              },
+                      ))),
+              Padding(padding: EdgeInsets.only(bottom: 20)),
+              //button submit
+              Container(
+                margin: EdgeInsets.only(right: 30),
+                alignment: Alignment.centerRight,
+                child: MaterialButton(
+                  height: 45,
+                  onPressed: () => {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => Main()))
+                  },
+                  child: Image.asset('assets/images/submit.png', width: 45),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 162)),
+              //Bottom text
+              RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  text: 'Don''t have an account?',
+                  style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                      color: Colors.white),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '\nSign Up',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontSize: 18,
                       ),
-                          ],
-                        ),
-                      ),
-                Padding(padding: EdgeInsets.only(bottom: 31.5)),
-              ],
-            ),
-          )
-        ),
+                      recognizer: new TapGestureRecognizer()
+                        ..onTap = () => {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          SignUpScreen()))
+                            },
+                    ),
+                  ],
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 31.5)),
+            ],
+          ),
+        )),
       ),
     );
   }
